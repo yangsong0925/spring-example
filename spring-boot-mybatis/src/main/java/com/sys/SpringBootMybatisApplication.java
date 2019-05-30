9c251604f2ee24af9883aa6f3e8f84e17348185d
yangsong0925 <yangsong563917>
2019-05-31 01:28:30 +0800

    config

--- spring-boot-mybatis/src/main/java/com/sys/SpringBootMybatisApplication.java
+++ spring-boot-mybatis/src/main/java/com/sys/SpringBootMybatisApplication.java
@@ -1,29 +1,20 @@
 package com.sys;
 
-import com.sys.interceptor.CustomHandlerInterceptor;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication()
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.sys"})
 public class SpringBootMybatisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMybatisApplication.class, args);
     }
 
-//    @Bean
-//    public WebMvcConfigurerAdapter customMvcConfigurerAdapter (){
-//        return new WebMvcConfigurerAdapter() {
-//            @Override
-//            public void addInterceptors(InterceptorRegistry registry) {
-//                registry.addInterceptor(new CustomHandlerInterceptor()).addPathPatterns("/**");
-//                // 添加拦截器
-//                super.addInterceptors(registry);
-//            }
-//        };
-//    }
 
 }
