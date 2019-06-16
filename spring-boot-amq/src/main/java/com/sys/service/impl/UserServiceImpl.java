0e0a726b2c852a327029fe5fd5db207db7b4b8e7
yangsong0925 <yangsong563917>
2019-06-16 12:58:52 +0800

    init

---
+++ spring-boot-amq/src/main/java/com/sys/service/impl/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.sys.service.impl;
+
+import com.sys.mybatis.entity.User;
+import com.sys.mybatis.mapper.UserMapper;
+import com.sys.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author ：yangsong
+ * @date ：Created in 2019年5月21日 0021 00:10
+ * @description：${description}
+ * @modified By：
+ * @version: $version$
+ */
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class UserServiceImpl implements UserService {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public List<User> findAll() {
+        return userMapper.findAll();
+    }
+
+    @Override
+    public int deleteUser(int id) {
+        return userMapper.delete(id);
+    }
+}
