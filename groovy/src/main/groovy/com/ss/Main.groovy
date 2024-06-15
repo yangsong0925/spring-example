ad4c0be8a58866a45b9c9254d37af647894cee29
yangsong <13980152803@163.com>
2024-06-15 11:46:11 +0800

    清除无用代码

---
+++ groovy/src/main/groovy/com/ss/Main.groovy
@@ -0,0 +1,20 @@
+package com.ss
+
+def username = "打"
+
+printf(username)
+
+def map = [:]
+
+map."an identifier with a space and double quotes" = "ALLOWED"
+map.'with-dash-signs-and-single-quotes' = "ALLOWED"
+
+assert map."an identifier with a space and double quotes" == "ALLOWED"
+assert map.'with-dash-signs-and-single-quotes' == "ALLOWED"
+
+map.'single quote'
+map."double quote"
+map.'''triple single quote'''
+map."""triple double quote"""
+map./slashy string/
+map.$/dollar slashy string/$
---
+++ groovy/src/main/groovy/com/ss/Test.java
@@ -0,0 +1,13 @@
+package com.ss;
+
+/**
+ * @author yangsong
+ * @date 2024/6/12 上午11:13
+ * @Description:
+ * @version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
